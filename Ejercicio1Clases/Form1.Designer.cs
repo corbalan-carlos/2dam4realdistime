347fc9026ec33961a24583ead08ec9729233acb3
Carlos <)>
2024-11-26 22:22:44 +0100

    idk

---
+++ Ejercicio1Clases/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Ejercicio1Clases
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.altasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pedidosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.porCodigoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.porNombreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton3 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.minimosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.porCategoriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.todosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton4 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripDropDownButton2,
+            this.toolStripDropDownButton3,
+            this.toolStripDropDownButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.altasToolStripMenuItem,
+            this.pedidosToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(67, 22);
+            this.toolStripDropDownButton1.Text = "Articulos";
+            this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click_1);
+            // 
+            // altasToolStripMenuItem
+            // 
+            this.altasToolStripMenuItem.Name = "altasToolStripMenuItem";
+            this.altasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.altasToolStripMenuItem.Text = "Altas";
+            // 
+            // pedidosToolStripMenuItem
+            // 
+            this.pedidosToolStripMenuItem.Name = "pedidosToolStripMenuItem";
+            this.pedidosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pedidosToolStripMenuItem.Text = "Pedidos";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // toolStripDropDownButton2
+            // 
+            this.toolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.porCodigoToolStripMenuItem,
+            this.porNombreToolStripMenuItem});
+            this.toolStripDropDownButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton2.Image")));
+            this.toolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+            this.toolStripDropDownButton2.Size = new System.Drawing.Size(72, 22);
+            this.toolStripDropDownButton2.Text = "Consultas";
+            // 
+            // porCodigoToolStripMenuItem
+            // 
+            this.porCodigoToolStripMenuItem.Name = "porCodigoToolStripMenuItem";
+            this.porCodigoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.porCodigoToolStripMenuItem.Text = "Por Codigo";
+            // 
+            // porNombreToolStripMenuItem
+            // 
+            this.porNombreToolStripMenuItem.Name = "porNombreToolStripMenuItem";
+            this.porNombreToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.porNombreToolStripMenuItem.Text = "Por Nombre";
+            // 
+            // toolStripDropDownButton3
+            // 
+            this.toolStripDropDownButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton3.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.minimosToolStripMenuItem,
+            this.porCategoriaToolStripMenuItem,
+            this.todosToolStripMenuItem});
+            this.toolStripDropDownButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton3.Image")));
+            this.toolStripDropDownButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton3.Name = "toolStripDropDownButton3";
+            this.toolStripDropDownButton3.Size = new System.Drawing.Size(63, 22);
+            this.toolStripDropDownButton3.Text = "Listados";
+            // 
+            // minimosToolStripMenuItem
+            // 
+            this.minimosToolStripMenuItem.Name = "minimosToolStripMenuItem";
+            this.minimosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.minimosToolStripMenuItem.Text = "Minimos";
+            // 
+            // porCategoriaToolStripMenuItem
+            // 
+            this.porCategoriaToolStripMenuItem.Name = "porCategoriaToolStripMenuItem";
+            this.porCategoriaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.porCategoriaToolStripMenuItem.Text = "Por categoria";
+            // 
+            // todosToolStripMenuItem
+            // 
+            this.todosToolStripMenuItem.Name = "todosToolStripMenuItem";
+            this.todosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.todosToolStripMenuItem.Text = "Todos";
+            // 
+            // toolStripDropDownButton4
+            // 
+            this.toolStripDropDownButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton4.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem});
+            this.toolStripDropDownButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton4.Image")));
+            this.toolStripDropDownButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton4.Name = "toolStripDropDownButton4";
+            this.toolStripDropDownButton4.Size = new System.Drawing.Size(72, 22);
+            this.toolStripDropDownButton4.Text = "Acerca de";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.acercaDeToolStripMenuItem.Text = "Autor";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 409);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem altasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pedidosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+        private System.Windows.Forms.ToolStripMenuItem porCodigoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem porNombreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton3;
+        private System.Windows.Forms.ToolStripMenuItem minimosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem porCategoriaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem todosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton4;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
--- a/src/a/Main.java
+++ a/src/a/Main.java
@@ -3,40 +3,24 @@ package a;
 import java.util.Arrays;
 
 public class Main {
+	static final int[][] mults= {{6*6*6*36,6*6*36,6*36},{36,6,1}};
 	static final int[][] startingArr= {{1,2,3},{4,5,0}};
-	static Tree root;
-	static Main.Tuple[] hash=new Tuple[46656];
+	static  int[][] hash=new int[6][60];
 	public static void main(String[] args) throws InterruptedException {
-		int n=getHash(startingArr);
+		int n=getAsInt(startingArr);
 		//Thread.sleep(30000);
-		root=new Main.Tree();
-		root.a=n;
-		root.branches=new Tree[4];
-		root.branches[0]=new Tree();
-		root.branches[1]=new Tree();
-		root.branches[2]=new Tree();
-		root.branches[3]=new Tree();
-		createTree(1,2,startingArr,root,0);
+		//createTree(1,2,n,0);
 		//runTTree(root, 0);
-		int i;
+	/*	int i;
 		int j =0;
 		for (i=0;i<46656;i++ ) {
+			if (hash[i]==null) continue;
 			if (hash[i].a) {
-				System.out.print("permutacion "+i+ " equivalente al puzzle "+ pToString(i)+"\n");
+				System.out.print("permutacion "+i+ " equivalente al puzzle "+ pToString(i)+" a la profundidad " +hash[i].b+"\n");
 				j++;
 			}
 		}
 		System.out.print("N total de permutaciones: "+j+"\n");
-	}
-	static void bRunTTree (Tree branch,int c) {
-		
-	}
-	static void runTTree (Tree branch,int d) {
-		if (branch==null) return;
-		for (Tree a:branch.branches) {
-			runTTree(a,d+1);
-		}
-		System.out.print(branch.a+" at depth "+d+" with puzzle "+pToString(branch.a)+"\n");
 	}
 	static String pToString(int a) {
 		if (a==-1||a==0) return "Puzzle no asignable";
@@ -50,66 +34,50 @@ public class Main {
 		}
 		return c.toString();
 	}
-	static int createTree(int x,int y,int[][] arr,Tree branch,int depth) {
-		int n=getHash(arr);
-		if (hash[n].a) {
-			if (hash[n].b>depth) return n;
-		} else {
-			hash[n].a=true;
+	/*static void createTree(int x,int y,int n,int depth) {
+		
+			int buf;
+			if (y<2) {
+				buf=n;
+				n/=mults[x][y+1];
+				n%=6;
+				n=buf-n*(mults[x][y+1])+(n*mults[x][y]);
+				
+				n=buf;
+			} 
+			if (y>0) {
+				buf=n;
+				n/=mults[x][y-1];
+				n%=6;
+				n=buf-n*(mults[x][y-1])+n*(mults[x][y]);
+				createTree(x,y-1,n,depth+1);
+				n=buf;
+			}
+			if (x==0) {
+				buf=n;
+				n/=mults[1][y];
+				n%=6;
+				n=buf+(n*mults[x][y])-n*(mults[1][y]);
+				createTree(1,y,n,depth+1);
+				n=buf;
+			}
+			if (x==1) {
+				buf=n;
+				n/=mults[0][y];
+				n%=6;
+				n=buf+(n*mults[x][y])-n*(mults[0][y]);
+				createTree(0,y,n,depth+1);
+				n=buf;
+			}
 		}
-		int[] a=new int[4];
-		int buf;
-		branch.a=n;
-		branch.branches=new Tree[4];
-		branch.branches[0]=new Tree();
-		branch.branches[1]=new Tree();
-		branch.branches[2]=new Tree();
-		branch.branches[3]=new Tree();
-		if (y<2) {
-			buf=arr[x][y+1];
-			arr[x][y+1]=0;
-			arr[x][y]=buf;
-			a[0]=createTree(x,y+1,arr,branch.branches[0],depth+1);
-			arr[x][y+1]=buf;
-			arr[x][y]=0;
-		} 
-		if (y>0) {
-			buf=arr[x][y-1];
-			arr[x][y-1]=0;
-			arr[x][y]=buf;
-			a[1]=createTree(x,y-1,arr,branch.branches[1],depth+1);
-			arr[x][y-1]=buf;
-			arr[x][y]=0;
-		}
-		if (x==0) {
-			buf=arr[1][y];
-			arr[1][y]=0;
-			arr[0][y]=buf;
-			a[2]=createTree(1,y,arr,branch.branches[2],depth+1);
-			arr[0][y]=0;
-			arr[1][y]=buf;
-		}
-		if (x==1) {
-			buf=arr[0][y];
-			arr[0][y]=0;
-			arr[1][y]=buf;
-			a[3]=createTree(0,y,arr,branch.branches[3],depth+1);
-			arr[1][y]=0;
-			arr[0][y]=buf;
-		}
-		branch.branches[0].a=a[0];
-		branch.branches[1].a=a[1];
-		branch.branches[2].a=a[2];
-		branch.branches[3].a=a[3];
-		return n;
-	}
-	static int[][] unhash(int hash) {
+		return;
+	}*/
+	static int[][] unInt(int hash) {
 		int[] a=new int[6];
 		int[][] r=new int[2][3];
-		int j=0;
-		int k=1;
+		int j=5;
 		for (int i=7776;i>0;i/=6) {
-			a[j++]=hash/i;
+			a[j--]=hash/i;
 			hash%=i;
 		}
 		r[0][0]=a[5];
@@ -120,26 +88,27 @@ public class Main {
 		r[1][2]=a[0];
 		return r;
 	}
-	static int getHash(int[][] hasheable) {
+	static int getAsInt(int[][] hasheable) {
 			int r;
 			int c=1;
-			r=hasheable[0][0]*(c);
-			r+=hasheable[0][1]*(c*=6);
-			r+=hasheable[0][2]*(c*=6);
-			r+=hasheable[1][0]*(c*=6);
+			r=hasheable[1][2]*(c);
 			r+=hasheable[1][1]*(c*=6);
-			r+=hasheable[1][2]*(c*=6);
+			r+=hasheable[1][0]*(c*=6);
+			r+=hasheable[0][2]*(c*=6);
+			r+=hasheable[0][1]*(c*=6);
+			r+=hasheable[0][0]*(c*=6);
 			return r;
 	}
-	static public class Tree {
-		int a;
-		Tree[] branches;
-		public Tree() {
-			branches=new Tree[4];
-		}
+	static void putHash(int[][] hasheable) {
+		int r=hasheable[0][0];
+		hash[r]
 	}
 	static class Tuple {
 		boolean a;
 		int b;
+		Tuple (int n) {
+			this.b=n;
+			a=true;
+		}
 	}
 }
