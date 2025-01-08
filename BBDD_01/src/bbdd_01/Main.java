fa41f56b340e768ba8a0217dc9fb9aa8ffdd6d5c
Carlos <)>
2025-01-08 21:09:12 +0100

    eepy

--- BBDD_01/src/bbdd_01/Main.java
+++ BBDD_01/src/bbdd_01/Main.java
@@ -4,12 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.mysql.cj.jdbc.ConnectionGroup;
+
 public class Main {
 
-	static public void main(String[] args) {
+	static public void main(String[] args) throws ClassNotFoundException {
 		
-		try (Connection conn=DriverManager.getConnection("localhost", "emp_dpt", "pass")){ 
-			System.out.println("Conexion hechar correctamente");
+		try (Connection conn=DriverManager.getConnection("jdbc:mysql://localhost", "user", "pass")){
+			System.out.println("Conexion hecha correctamente");
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.exit(1);
