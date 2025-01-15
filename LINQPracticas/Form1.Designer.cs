43a9f60c982d4ff5a4a180790e6366855df6908c
a <a@clase>
2025-01-15 11:35:00 +0100

    Mas LINQ

--- LINQPracticas/Form1.Designer.cs
+++ LINQPracticas/Form1.Designer.cs
@@ -32,6 +32,7 @@
             listBox2 = new ListBox();
             label1 = new Label();
             listBox3 = new ListBox();
+            label2 = new Label();
             SuspendLayout();
             // 
             // listBox1
@@ -55,11 +56,12 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(412, 12);
+            label1.Location = new Point(462, 42);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 2;
             label1.Text = "label1";
+            label1.Click += label1_Click;
             // 
             // listBox3
             // 
@@ -67,14 +69,24 @@
             listBox3.ItemHeight = 15;
             listBox3.Location = new Point(286, 12);
             listBox3.Name = "listBox3";
-            listBox3.Size = new Size(120, 409);
+            listBox3.Size = new Size(170, 409);
             listBox3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(462, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(143, 30);
+            label2.TabIndex = 4;
+            label2.Text = "String: \"hola como estas\" \r\nLetra a buscar :'a'\r\n";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
             Controls.Add(listBox3);
             Controls.Add(label1);
             Controls.Add(listBox2);
@@ -91,5 +103,6 @@
         private ListBox listBox2;
         private Label label1;
         private ListBox listBox3;
+        private Label label2;
     }
 }
--- LINQPracticas/Form1.cs
+++ LINQPracticas/Form1.cs
@@ -1,6 +1,5 @@
 using System.Linq;
 using System.Linq.Expressions;
-using 
 
 namespace LINQPracticas
 {
@@ -9,13 +8,13 @@ namespace LINQPracticas
         private List<int> GetEven(int n2)
         {
             List<int> iterable = new List<int>(n2);
-            for (int i=0;i<=n2;i++)
+            for (int i = 0; i <= n2; i++)
             {
                 iterable.Add(i);
             }
             var returnable =
                 from i in iterable
-                where (i|1) == i+1
+                where (i | 1) == i + 1
                 select i;
             foreach (var item in returnable)
             {
@@ -31,17 +30,34 @@ namespace LINQPracticas
               where (i == l)
               select i).Count<char>();
         }
-        string[] GroupByLegth(string[] words)
+        string[] GroupByLength(string[] words)
         {
+            var a =
+                from w in words
+                orderby w.Length
+                group w by w.Length;
+            String[] returnable = new string[words.Length];
+            int c = 0;
+            foreach (var i in a)
+            {
+
+                foreach (var j in i)
+                {
+                    returnable[c++] = $"Longitud {i.Key}: {j}";
+                }
+            }
+            return returnable;
+
         }
+
         public Form1()
         {
             InitializeComponent();
-            foreach (var item in GetEven( 50))
+            foreach (var item in GetEven(50))
             {
-            listBox1.Items.Add(item); 
+                listBox1.Items.Add(item);
             }
-            var iterable  = new[] 
+            var iterable = new[]
             {
                 new {nombre = "Pepe", nota =5},
                 new {nombre = "Juan", nota =8},
@@ -55,11 +71,62 @@ namespace LINQPracticas
                 select i;
             foreach (var i in returnble)
             {
-                listBox1.Items.Add(i);
+                listBox2.Items.Add(i);
             }
+
             label1.Text =
                 LetterCount('a', "hola que tal estas").ToString();
-            
+            foreach (string i in GroupByLength(["hola", "como", "estas", "bien", "tu", "que", "tal"]))
+            {
+                listBox3.Items.Add(i);
+            }
+            Productos[] tienda1 = [new Productos("kk", 10, 10), new Productos("Hola", 2, 4)];
+            Productos[] tienda2 = [new Productos("ll", 4, 8), new Productos("adios", 100, 0)];
+            Productos[] tienda3 = [new Productos("qq", 1, 2), new Productos("saluaciones", 3, 4)];
+            Tienda[] b = [
+                new Tienda("tienda1",tienda1 ),
+                new Tienda("tienda1",tienda1),
+                new Tienda("tienda1",tienda1)
+
+                ];
+             decimal[] ingresosPorTienda =
+               (from t in b
+               select t.p into a
+               select a.Sum(myVar => myVar.precio)).ToArray();
+
+            Productos[] masVendidos = 
+                (from t in b
+                 select t.p into a
+                 select  
+                 )
+        }
+        class Tienda
+        {
+            public string tienda;
+            public Productos[] p;
+
+            public Tienda(string tienda, Productos[] p)
+            {
+                this.tienda = tienda;
+                this.p = p;
+            }
+        }
+        class Productos
+        {
+public            string nombre;
+     public       decimal precio;
+          public  uint nvendidas;
+
+            public Productos(string nombre, decimal precio, uint nvendidas)
+            {
+                this.nombre = nombre;
+                this.precio = precio;
+                this.nvendidas = nvendidas;
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
